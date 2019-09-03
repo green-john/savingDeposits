be4c5bfba11ec7204f0c2e0cbd3628c486f744e9
Andrés Ruiz <andresfeliperu@gmail.com>
2019-09-04 00:24:25 +0200

    Fix tests

--- scripts/all_tests.sh
+++ scripts/all_tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd ..
-export RENTALS_DB_HOST=localhost
-export RENTALS_DB_NAME=savings-dev
-export RENTALS_DB_USER=juan
+export SAVINGS_DB_HOST=localhost
+export SAVINGS_DB_NAME=savings-test
+export SAVINGS_DB_USER=afruizc
 go test ./...
 
